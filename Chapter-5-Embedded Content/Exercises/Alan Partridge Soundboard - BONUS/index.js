a719ce72f9874490f941d8f7ae8311fed41ceb1c
M.Tayyeb <tayyeb0750@gmail.com>
2024-04-12 19:50:51 +0400

    Alan Partridge Soundboard - BONUS

---
+++ Chapter-5-Embedded Content/Exercises/Alan Partridge Soundboard - BONUS/index.js
@@ -0,0 +1,41 @@
+// Creating a variable called audio1 then getting it from the main html file
+var audio1 = document.getElementById("audio1");
+// Making the function on each button that just plays the audio this is done for all of the respective buttons and their audios
+function playAudio1() {
+  audio1.play();
+}
+
+var audio2 = document.getElementById("audio2");
+function playAudio2() {
+  audio2.play();
+}
+
+var audio3 = document.getElementById("audio3");
+function playAudio3() {
+  audio3.play();
+}
+
+var audio4 = document.getElementById("audio4");
+function playAudio4() {
+  audio4.play();
+}
+
+var audio5 = document.getElementById("audio5");
+function playAudio5() {
+  audio5.play();
+}
+
+var audio6 = document.getElementById("audio6");
+function playAudio6() {
+  audio6.play();
+}
+
+var audio7 = document.getElementById("audio7");
+function playAudio7() {
+  audio7.play();
+}
+
+var audio8 = document.getElementById("audio8");
+function playAudio8() {
+  audio8.play();
+}
